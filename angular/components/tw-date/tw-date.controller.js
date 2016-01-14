b3a57a50ab6dd080b956fd4ccde7e47381be915b
Steve Pole <steve.pole@transferwise.com>
2016-01-14 12:14:18 +0000

    Add work in progress date input

---
+++ angular/components/tw-date/tw-date.controller.js
@@ -0,0 +1,199 @@
+(function(angular) {
+	'use strict';
+
+	angular
+		.module('tw.form-components')
+		.controller('TwDateController', TwDateController);
+
+	TwDateController.$inject = ['$scope', '$element'];
+
+	function TwDateController($scope, $element) {
+		var vm = this;
+
+		vm.init = init;
+		vm.explodeDate = explodeDate;
+		vm.updateDate = updateDate;
+		vm.correctHighDay = correctHighDay;
+
+		vm.pad = pad;
+		vm.validDate = validDate;
+
+		function init() {
+			vm.month = 1;
+
+			vm.dateMode = typeof vm.date;
+
+			$scope.$watch('vm.date', function(date) {
+				vm.explodeDate(date);
+			});
+
+			vm.explodeDate(vm.date);
+
+			vm.months = getMonths(vm.locale);
+
+			// If attribute has no value make it evaluate to true
+			if (vm.required === "") {
+				vm.required = true;
+			}
+		}
+
+		function getMonths(locale) {
+			var date = new Date(), monthName;
+
+			if (!date.toLocaleString) {
+				// Browser doesn't support toLocaleString, use English
+				return getEnglishMonths();
+			}
+
+			if (!locale) {
+				locale = 'en-GB';
+			}
+
+			var safariRegex = /^[0-9]{1,2}\s[a-zA-Z]+\s[0-9]{4}\s[0-9]{2}:[0-9]{2}:[0-9]{2}\s[A-Z]{3}/;
+
+			// Some local strings can throw error
+			try {
+				var testToLocaleString = date.toLocaleString(locale, {month: "long"});
+
+				if (safariRegex.test(testToLocaleString)) {
+					return getEnglishMonths();
+				}
+			} catch(ex) {
+				return getEnglishMonths();
+			}
+
+			var months = [];
+			for(var i = 0; i < 12; i++) {
+				date.setMonth(i);
+				monthName = date.toLocaleString(locale, {month: "long"});
+
+
+
+				months.push({
+					id: i + 1,
+					name: monthName.charAt(0).toUpperCase() + monthName.slice(1)
+				});
+			}
+			return months;
+		}
+
+		function getEnglishMonths() {
+			return [
+				{id: 1, name: "January"},
+				{id: 2, name: "February"},
+				{id: 3, name: "March"},
+				{id: 4, name: "April"},
+				{id: 5, name: "May"},
+				{id: 6, name: "June"},
+				{id: 7, name: "July"},
+				{id: 8, name: "August"},
+				{id: 9, name: "September"},
+				{id: 10, name: "October"},
+				{id: 11, name: "November"},
+				{id: 12, name: "December"}
+			];
+		}
+
+		function explodeDate(date) {
+			var dateObj;
+			if (!date) {
+				return;
+			}
+
+			if (typeof date === "string") {
+				dateObj = new Date(date);
+			} else {
+				dateObj = date;
+			}
+
+			if (!validDate(dateObj)) {
+				vm.day = null;
+				vm.month = 1;
+				vm.year = null;
+				return;
+			}
+
+			vm.day = dateObj.getUTCDate();
+			vm.month = dateObj.getUTCMonth() + 1;
+			vm.year = dateObj.getUTCFullYear();
+		}
+
+		function updateDate() {
+			var dateObj, dateFormatted;
+
+			if (!vm.day ||
+				vm.month === null ||
+				vm.month === undefined ||
+				!vm.year) {
+				vm.date = null;
+
+				$element.addClass("ng-invalid-pattern");
+
+				return;
+			}
+
+			vm.day = vm.correctHighDay(vm.day, vm.month, vm.year);
+
+			dateObj = new Date(Date.UTC(
+				Number(vm.year),
+				Number(vm.month) - 1,
+				Number(vm.day)
+			));
+
+			/**/
+			var minObj = new Date(vm.ngMin);
+			var maxObj = new Date(vm.ngMax);
+
+			console.log(vm.ngMin);
+			console.log(vm.ngMax);
+			console.log(minObj);
+			console.log(maxObj);
+			console.log(dateObj);
+
+			if (dateObj < minObj) {
+				$element.addClass("ng-invalid-min");
+				return;
+			}
+			if (dateObj > maxObj) {
+				$element.addClass("ng-invalid-max");
+				return;
+			}
+			/**/
+
+			$element.removeClass("ng-invalid-min");
+			$element.removeClass("ng-invalid-max");
+			$element.removeClass("ng-invalid-pattern");
+
+			if (vm.dateMode === "string") {
+				vm.date = dateObj.getUTCFullYear()
+					+ '-' + pad(dateObj.getUTCMonth() + 1)
+					+ '-' + pad(dateObj.getUTCDate());
+			} else {
+				vm.date = dateObj;
+			}
+		}
+
+		function pad(n) {
+			return (n < 10) ? '0' + n : n;
+		}
+
+		function validDate(dateObj) {
+			return Object.prototype.toString.call(dateObj) === "[object Date]"
+				&& !isNaN(dateObj.getTime());
+		}
+
+		function correctHighDay(day, month, year) {
+			var dateObj = new Date(0);
+			dateObj.setUTCFullYear(year);
+			dateObj.setUTCMonth(month); // for month, 0=Jan
+			dateObj.setUTCDate(0);
+			if (day > dateObj.getUTCDate()) {
+				return dateObj.getUTCDate();
+			}
+			return day;
+		}
+
+		init();
+	}
+
+})(window.angular);
---
+++ angular/components/tw-date/tw-date.directive.js
@@ -0,0 +1,98 @@
+(function(angular) {
+	'use strict';
+
+	angular
+		.module('tw.form-components')
+		.directive('twInputDate', TwDateDirective);
+
+	function TwDateDirective() {
+		var directive = {
+			bindToController: true,
+			controller: "TwDateController",
+			controllerAs: 'vm',
+			replace: true,
+			restrict: 'E',
+			scope: {
+				date: '=ngModel',
+				ngMin: '=ngMin',
+				ngMax: '=ngMax',
+				disabled: '@',
+				required: '@',
+				locale: '@'
+			},
+			template: "<div class='row'> \
+				<div class='col-sm-3'> \
+					<label class='sr-only' for='day-{{::uniqueId}}'>Day</label> \
+					<input type='number' \
+						name='day' \
+						id='day-{{::uniqueId}}' \
+						class='form-control' \
+						ng-model='vm.day' \
+						ng-change='vm.updateDate()' \
+						placeholder='DD' \
+						min='1' \
+						max='31' \
+						maxlength='2' \
+						ng-min='1' \
+						ng-max='31' \
+						ng-maxlength='2' \
+						ng-disabled='vm.disabled' \
+						ng-required='vm.required' \
+						tw-validation /> \
+				</div> \
+				<div class='col-sm-5'> \
+					<label class='sr-only' for='month-{{::uniqueId}}'>Month</label> \
+					<select name='month' \
+						id='month-{{::uniqueId}}' \
+						class='form-control' \
+						ng-model='vm.month' \
+						ng-change='vm.updateDate()' \
+						ng-options='month.id as month.name for month in vm.months' \
+						ng-disabled='vm.disabled' \
+						required \
+						autocomplete='off' \
+						tw-validation> \
+					</select> \
+				</div> \
+				<div class='col-sm-4'> \
+					<label class='sr-only' for='year-{{::uniqueId}}'>Year</label> \
+					<input type='number' \
+						id='year-{{::uniqueId}}' \
+						name='year' \
+						class='form-control' \
+						placeholder='YYYY' \
+						ng-model='vm.year' \
+						ng-change='vm.updateDate()' \
+						min='1900' \
+						max='2015' \
+						maxlength='4' \
+						ng-maxlength='4' \
+						ng-disabled='vm.disabled' \
+						ng-required='vm.required' \
+						tw-validation /> \
+				</div> \
+			</div>",
+			link: TwDateLink
+		};
+
+		return directive;
+	}
+
+	function TwDateLink(scope, element, attrs, ctrl) {
+		if (attrs.ngDisabled) {
+			scope.$parent.$watch(attrs.ngDisabled, function(isDisabled) {
+				scope.disabled = isDisabled;
+			});
+		}
+
+		if (attrs.ngRequired) {
+			scope.$parent.$watch(attrs.ngRequired, function(isRequired) {
+				scope.required = isRequired;
+			});
+		}
+
+		element.find("[name=month]").change(function() {
+			element.find("[name=day]").focus().blur();
+		});
+	}
+})(window.angular);
