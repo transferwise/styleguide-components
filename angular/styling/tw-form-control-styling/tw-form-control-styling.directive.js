c19801da4893c2205cda1e03df7b2bd763e5c065
Steve Pole <steve.pole@transferwise.com>
2016-04-01 21:54:15 +0100

    Add focusable directive

--- angular/styling/tw-form-control-styling/tw-form-control-styling.directive.js
+++ angular/styling/tw-form-control-styling/tw-form-control-styling.directive.js
@@ -8,17 +8,30 @@
 	function TwFormControlStyling() {
 		return {
 			restrict: 'C',
-			link: function(scope, element) {
-				var formGroup = $(element).closest('.form-group');
-
-				$(element)
-					.on('focus', function() {
-						formGroup.addClass('focus');
-					})
-					.on('blur', function() {
-						formGroup.removeClass('focus');
-					});
-			}
+			link: FocusableLink
 		};
+	}
+
+	angular
+		.module('tw.form-styling')
+		.directive('twFocusable', TwFocusable);
+
+	function TwFocusable() {
+		return {
+			restrict: 'A',
+			link: FocusableLink
+		};
+	}
+
+	function FocusableLink(scope, element) {
+		var formGroup = $(element).closest('.form-group');
+
+		$(element)
+			.on('focus', function() {
+				formGroup.addClass('focus');
+			})
+			.on('blur', function() {
+				formGroup.removeClass('focus');
+			});
 	}
 })(window.angular);
