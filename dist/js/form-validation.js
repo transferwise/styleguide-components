3182860f5d7ca3f3fe79639b921b6060d81bc21e
Steve Pole <steve.pole@transferwise.com>
2016-06-24 10:33:10 +0100

    Update docs

--- dist/js/form-validation.js
+++ dist/js/form-validation.js
@@ -6,47 +6,14 @@ angular.module("tw.form-validation", []);
             restrict: "E",
             link: function(scope, element) {
                 $(element).on("submit", function() {
-                    $(element).find("[tw-validation].ng-invalid").closest(".form-group").addClass("has-error");
-                    var invalidControl = $(element).find("input[type=checkbox].ng-invalid, input[type=radio].ng-invalid, .tw-checkbox.ng-invalid, .tw-radio.ng-invalid");
-                    return invalidControl.closest(".checkbox, .radio").addClass("has-error"), invalidControl.parents(".form-group").addClass("has-error"), 
+                    var elements = $(element).find("[tw-validation].ng-invalid");
+                    return elements.closest(".form-group").addClass("has-error"), elements.closest(".checkbox, .radio").addClass("has-error"), 
                     !0;
                 });
             }
         };
     }
     angular.module("tw.form-validation").directive("form", TwFormValidation);
-}(window.angular), function(angular) {
-    function TwInputValidation() {
-        function checkValid(input, label, formGroup) {
-            setTimeout(function() {
-                input.hasClass("ng-invalid") ? (label.addClass("has-error"), formGroup.addClass("has-error")) : (label.removeClass("has-error"), 
-                checkFormGroup(formGroup));
-            });
-        }
-        function checkFormGroup(formGroup) {
-            var formGroupInvalidInputs = formGroup.find("input.ng-invalid"), formGroupValidInputsContainers = formGroup.find("input.ng-valid").closest(".checkbox, .radio");
-            setTimeout(function() {
-                formGroupValidInputsContainers.removeClass("has-error"), 0 === formGroupInvalidInputs.length && formGroup.removeClass("has-error");
-            });
-        }
-        function link(scope, element, attrs, ctrl) {
-            if (attrs.type) {
-                var type = attrs.type.toLowerCase();
-                if (("radio" === type || "checkbox" === type) && 0 !== $(element).closest(labelSelector).length) {
-                    var formControl = $(element), label = formControl.closest("label");
-                    label.on("click", function() {
-                        checkValid(formControl, formControl.closest(".checkbox, .radio"), formControl.closest(".form-group"));
-                    });
-                }
-            }
-        }
-        var labelSelector = ".checkbox > label, .radio > label";
-        return {
-            restrict: "E",
-            link: link
-        };
-    }
-    angular.module("tw.form-validation").directive("input", TwInputValidation);
 }(window.angular), function(angular) {
     "use strict";
     function TwValidation() {
