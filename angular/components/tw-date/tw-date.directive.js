47fb371453181510be70096857367be6872c8479
Steve Pole <steve.pole@transferwise.com>
2016-10-19 12:11:01 +0100

    Remove some repetition in tw-date template.  Build.

--- angular/components/tw-date/tw-date.directive.js
+++ angular/components/tw-date/tw-date.directive.js
@@ -95,65 +95,64 @@
 		/**/
 	}
 
+	var daySectionTemplate = " \
+		<label class='sr-only' for='day-{{::uniqueId}}'>Day</label> \
+		<input type='number' \
+			name='day' \
+			id='day-{{::uniqueId}}' \
+			class='form-control tw-date-day' \
+			ng-model='vm.day' \
+			ng-change='vm.updateDateModelAndValidationClasses()' \
+			placeholder='DD' \
+			min='1' \
+			ng-min='1' \
+			ng-disabled='vm.dateDisabled' \
+			ng-required='vm.dateRequired' \
+			tw-focusable />";
+
+	var monthSectionTemplate = "  \
+		<label class='sr-only' for='month-{{::uniqueId}}'>Month</label>\
+		<tw-select \
+			name='month' \
+			class='tw-date-month' \
+			id='month-{{::uniqueId}}' \
+			ng-model='vm.month' \
+			ng-change='vm.updateDateModelAndValidationClasses()' \
+			ng-required='vm.dateRequired' \
+			ng-disabled='vm.dateDisabled' \
+			options='vm.dateMonths'> \
+		</tw-select>";
+
+	var yearSectionTemplate = " \
+		<label class='sr-only' for='year-{{::uniqueId}}'>Year</label> \
+		<input type='number' \
+			id='year-{{::uniqueId}}' \
+			name='year' \
+			class='form-control tw-date-year' \
+			placeholder='YYYY' \
+			ng-model='vm.year' \
+			ng-change='vm.updateDateModelAndValidationClasses()' \
+			ng-min='vm.dateRange.min.getFullYear()' \
+			ng-max='vm.dateRange.max.getFullYear()' \
+			maxlength='4' \
+			ng-maxlength='4' \
+			ng-disabled='vm.dateDisabled' \
+			ng-required='vm.dateRequired' \
+			tw-focusable />";
+
 	var templateAsString = " \
-			<div class='row'> \
-				<div class='col-sm-5 tw-date-month-column' ng-if='vm.monthBeforeDay'> \
-					<label class='sr-only' for='month-{{::uniqueId}}'>Month</label> \
-					<tw-select \
-						name='month' \
-						class='tw-date-month' \
-						id='month-{{::uniqueId}}' \
-						ng-model='vm.month' \
-						ng-change='vm.updateDateModelAndValidationClasses()' \
-						ng-required='vm.dateRequired' \
-						ng-disabled='vm.dateDisabled' \
-						options='vm.dateMonths'> \
-					</tw-select> \
-				</div> \
-				<div class='col-sm-3 tw-date-day-column'> \
-					<label class='sr-only' for='day-{{::uniqueId}}'>Day</label> \
-					<input type='number' \
-						name='day' \
-						id='day-{{::uniqueId}}' \
-						class='form-control tw-date-day' \
-						ng-model='vm.day' \
-						ng-change='vm.updateDateModelAndValidationClasses()' \
-						placeholder='DD' \
-						min='1' \
-						ng-min='1' \
-						ng-disabled='vm.dateDisabled' \
-						ng-required='vm.dateRequired' \
-						tw-focusable /> \
-				</div> \
-				<div class='col-sm-5 tw-date-month-column' ng-if='!vm.monthBeforeDay'> \
-					<label class='sr-only' for='month-{{::uniqueId}}'>Month</label> \
-					<tw-select \
-						name='month' \
-						class='tw-date-month' \
-						id='month-{{::uniqueId}}' \
-						ng-model='vm.month' \
-						ng-change='vm.updateDateModelAndValidationClasses()' \
-						ng-required='vm.dateRequired' \
-						ng-disabled='vm.dateDisabled' \
-						options='vm.dateMonths'> \
-					</tw-select> \
-				</div> \
-				<div class='col-sm-4 tw-date-year-column'> \
-					<label class='sr-only' for='year-{{::uniqueId}}'>Year</label> \
-					<input type='number' \
-						id='year-{{::uniqueId}}' \
-						name='year' \
-						class='form-control tw-date-year' \
-						placeholder='YYYY' \
-						ng-model='vm.year' \
-						ng-change='vm.updateDateModelAndValidationClasses()' \
-						ng-min='vm.dateRange.min.getFullYear()' \
-						ng-max='vm.dateRange.max.getFullYear()' \
-						maxlength='4' \
-						ng-maxlength='4' \
-						ng-disabled='vm.dateDisabled' \
-						ng-required='vm.dateRequired' \
-						tw-focusable /> \
-				</div> \
-			</div>";
+		<div class='row'> \
+			<div class='col-sm-5 tw-date-month-column' ng-if='vm.monthBeforeDay'>" +
+				monthSectionTemplate + " \
+			</div> \
+			<div class='col-sm-3 tw-date-day-column'>" +
+				daySectionTemplate + " \
+			</div> \
+			<div class='col-sm-5 tw-date-month-column' ng-if='!vm.monthBeforeDay'>" +
+				monthSectionTemplate + " \
+			</div> \
+			<div class='col-sm-4 tw-date-year-column'>" +
+				yearSectionTemplate + " \
+			</div> \
+		</div>";
 })(window.angular);
