24516fa19ffa143ca39e778451aaeedb1aa461ef
Yash Nevatia <yash.nevatia@transferwise.com>
2017-06-23 17:09:01 +0100

    family of transclusions

--- angular/layout-components/tw-activity-card-container/tw-activity-card-container.directive.js
+++ angular/layout-components/tw-activity-card-container/tw-activity-card-container.directive.js
@@ -3,45 +3,62 @@
 
 	angular
 		.module('tw.layout-components')
-		.directive('twActivityCardRepeat', Repeat)
-		.directive('twActivityCardExpanded', Expanded)
-		.directive('twActivityCardCollapsed', Collapsed)
 		.directive('twActivityCard', Card)
-		.directive('twActivityCardContainer', CardContainer);
+		.component('twActivityCardContainer', CardContainer);
 
-	function Repeat() {
+	function Card() {
 		return {
-			transclude: true,
+			require: '^CardContainer',
+			scope: {
+				status: '@'
+			},
+			transclude: {
+				'collapsedCard': 'div',
+				'expandedCard': 'div',
+				'repeatCard': 'div'
+			},
+			controllerAs: '$ctrl';
+			controller: function () {
+				
+			}
 			template: ' \
-				<div class="well p-l-panel p-r-panel" \ ' +
-						// ng-if="$ctrl.canRepeat(transfer)"> \ <-- original line, below for testing
-						'ng-if="true"> \
+			<li 	class="list-group-item p-a-0" \
+					ng-class="{ \
+						\'list-group-item-danger\': transfer.status === \'PAUSED\', \
+						\'list-group-item-warning\': transfer.status === \'AWAITING_FUNDS\', \
+						\'list-group-item-info\': transfer.status === \'PROCESSING\' || transfer.status === \'FUNDED\', \
+						\'list-group-item-success\': transfer.status === \'PAID_OUT\', \
+						\'active\': $ctrl.active === transfer.id, \
+						\'disabled\': transfer.status === \'CANCELLED\' \
+					}"> \
+				<div class="p-a-panel" role="button" \
+					ng-click="$ctrl.activate(transfer.id, $event)"> \
 					<div class="media"> \
 						<div class="media-left"> \
-							<div class="circle circle-sm circle-responsive invisible"> \
-								<i class="icon icon-transfer"></i> \
+							<div class="circle circle-sm circle-responsive" ng-class="{ \
+								\'circle-inverse\': !$ctrl.inactive || (transfer.id === $ctrl.active)}"> \
+									<transfer-type-icon type="transfer.type"></transfer-type-icon> \
 							</div> \
 						</div> \
-						<div class="media-body"> \
-							<repeat-transfer \
-								source="transfer.source" \
-								target="transfer.target" \
-								source-amount="{{transfer.sourceAmount}}" \
-								target-amount="{{transfer.targetAmount}}" \
-								receiving-account="transfer.account.id" \
-								currencies="$ctrl.currencies"> \
-							</repeat-transfer> \
-							<div ng-transclude></div> \
+						<div class="media-body" ng-transclude="collapsedCard"></div> \
+						<div class="media-right hidden-xs hidden"> \
+							<div class="btn-group pull-xs-right"> \
+								<a href="" role="button" class="dropdown-toggle" \
+									data-toggle="dropdown" aria-expanded="false"> \
+									<div class="pips"> \
+										<div class="pip"></div> \
+										<div class="pip"></div> \
+										<div class="pip"></div> \
+									</div> \
+								</a> \
+								<ul class="dropdown-menu dropdown-menu-right" role="menu"> \
+									<li><a href="">Repeat</a></li> \
+									<li><a href="">View details</a></li> \
+								</ul> \
+							</div> \
 						</div> \
 					</div> \
-				</div>',
-		};
-	}
-
-	function Expanded() {
-		return {
-			transclude: true,
-			template: ' \
+				</div> \
 				<div 	class="collapse" \
 						ng-attr-aria-expanded="{{$ctrl.active === transfer.id}}" \
 						ng-class="{\'in\': $ctrl.active === transfer.id}" \
@@ -60,74 +77,41 @@
 								style="margin-left: -8px;"> \
 							<i class="icon icon-left-arrow icon-xxl"></i> \
 							</a> \
-							<div ng-transclude></div> \
+							<div ng-transclude="expandedCard"></div> \
 						</div> \
 					</div> \
-				</div>',
-		};
-	}
-
-	function Collapsed() {
-		return {
-			transclude: true,
-			template: ' \
-				<div class="p-a-panel" role="button" \
-					ng-click="$ctrl.activate(transfer.id, $event)"> \
+				</div> \
+				<div class="well p-l-panel p-r-panel" \ ' +
+						// ng-if="$ctrl.canRepeat(transfer)"> \ <-- original line, condition skipped for testing
+						'> \ 
 					<div class="media"> \
 						<div class="media-left"> \
-							<div class="circle circle-sm circle-responsive" ng-class="{ \
-								\'circle-inverse\': !$ctrl.inactive || (transfer.id === $ctrl.active)}"> \
-									<transfer-type-icon type="transfer.type"></transfer-type-icon> \
+							<div class="circle circle-sm circle-responsive invisible"> \
+								<i class="icon icon-transfer"></i> \
 							</div> \
 						</div> \
-						<div class="media-body" ng-transclude></div> \
-						<div class="media-right hidden-xs hidden"> \
-							<div class="btn-group pull-xs-right"> \
-								<a href="" role="button" class="dropdown-toggle" \
-									data-toggle="dropdown" aria-expanded="false"> \
-									<div class="pips"> \
-										<div class="pip"></div> \
-										<div class="pip"></div> \
-										<div class="pip"></div> \
-									</div> \
-								</a> \
-								<ul class="dropdown-menu dropdown-menu-right" role="menu"> \
-									<li><a href="">Repeat</a></li> \
-									<li><a href="">View details</a></li> \
-								</ul> \
-							</div> \
+						<div class="media-body"> \
+							<repeat-transfer \
+								source="transfer.source" \
+								target="transfer.target" \
+								source-amount="{{transfer.sourceAmount}}" \
+								target-amount="{{transfer.targetAmount}}" \
+								receiving-account="transfer.account.id" \
+								currencies="$ctrl.currencies"> \
+							</repeat-transfer> \
+							<div ng-transclude="repeatCard"></div> \
 						</div> \
 					</div> \
-				</div>',
-		};
-	}
-
-	function Card() {
-		return {
-			// require: '^ContainerController',
-			transclude: true,
-			template: ' \
-			<li 	class="list-group-item p-a-0" \
-					ng-class="{ \
-						\'list-group-item-danger\': transfer.status === \'PAUSED\', \
-						\'list-group-item-warning\': transfer.status === \'AWAITING_FUNDS\', \
-						\'list-group-item-info\': transfer.status === \'PROCESSING\' || transfer.status === \'FUNDED\', \
-						\'list-group-item-success\': transfer.status === \'PAID_OUT\', \
-						\'active\': $ctrl.active === transfer.id, \
-						\'disabled\': transfer.status === \'CANCELLED\' \
-					}"> \
-				<div ng-transclude></div> \
+				</div>, \
 			</li>',
 		};
 	}
 
 	function CardContainer() {
 		return {
-			controllerAs: 'ContainerController',
-			bindToController: true,
 			transclude: true,
-			template: '<div ng-transclude class="list-group panel-list-group list-group-slide-out" \
-							ng-class="{\'list-group-inactive\': $ctrl.inactive}"></div>',
+			template: '<ul ng-transclude class="list-group panel-list-group list-group-slide-out" \
+							ng-class="{\'list-group-inactive\': $ctrl.inactive}"></ul>',
 		};
 	}
 
