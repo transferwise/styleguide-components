d64670013cd1daa76b66f6db91a806f87e9323d5
Sofiia Horishna <sofiia.horishna@transferwise.com>
2017-08-22 11:30:26 +0300

    Fix

--- angular/services/tw-date-service/tw-date-service.service.js
+++ angular/services/tw-date-service/tw-date-service.service.js
@@ -84,7 +84,7 @@
 
 		this.getMonthNamesForLocale = function(locale, format) {
             var date, months = [], language = getLanguageFromLocale(locale);
-            if (DEFAULT_MONTH_NAMES_BY_LANGUAGE[language] && ((format === 'long') || !format)) {
+            if (DEFAULT_MONTH_NAMES_BY_LANGUAGE[language]) {
                 return DEFAULT_MONTH_NAMES_BY_LANGUAGE[language];
             }
 
