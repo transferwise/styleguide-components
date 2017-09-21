e93b0644034b0dca480e9688886af1d5b309297d
Steve Pole <steve.pole@transferwise.com>
2017-09-21 17:08:49 +0100

    Remove some karma conf duplication

--- karma-conf.js
+++ karma-conf.js
@@ -1,15 +1,4 @@
-let karmaConf = require('./karma.json');
-
-karmaConf.plugins = [
-  "karma-phantomjs-launcher",
-  "karma-jasmine",
-  "karma-coverage",
-  "karma-mocha-reporter"
-];
-
-karmaConf.browsers = [
-  "PhantomJS",
-]
+const karmaConf = require('./karma.json');
 
 module.exports = function(config) {
   config.set(karmaConf);
