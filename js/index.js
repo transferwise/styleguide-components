24b3fd34b3eabbffa948aa2bb8b762f8b79760da
Steve Pole <steve.pole@transferwise.com>
2017-10-06 12:24:24 +0100

    Update docs

--- js/index.js
+++ js/index.js
@@ -61,7 +61,7 @@ angular.module('tw.styleguide.docs', [])
   .directive('docsErrorMessages', () => ({
     replace: true,
     template: " \
-    <div class='error-messages'> \
+    <div class='alert alert-danger'> \
       <div class='error-required'>Required</div> \
       <div class='error-minlength'>Too short</div> \
       <div class='error-maxlength'>Too long</div> \
@@ -372,7 +372,8 @@ angular.module('tw.styleguide.docs', [])
         { value: 'account', label: 'Account' },
         { value: 'profile', label: 'Profile' },
         { value: 'verification', label: 'Verification' },
-        { value: 'pay-in', label: 'Pay in' }
+        { value: 'pay-in', label: 'Pay in' },
+        { value: 'transfer', label: 'Transfer' }
       ];
       $ctrl.type = 'profile';
       if (!$ctrl.model) {
@@ -380,6 +381,10 @@ angular.module('tw.styleguide.docs', [])
       }
 
       $ctrl.model.firstName = '01010101010';
+
+      $ctrl.onRefreshRequirements = function() {
+        console.log("on refresh requirements");
+      }
 
       $scope.$watch('$ctrl.type', (newVal) => {
         $http.get(`json/${newVal}-requirements.json`).then((response) => {
@@ -561,7 +566,6 @@ angular.module('tw.styleguide.docs', [])
                       <input class="form-control text-xs-right p-r-0" type="text"> \
                       <span class="input-group-addon p-l-1 ">USD</span> \
                   </div> \
-                  <div class="help-block m-b-0 ">Rate £1 = £1.2345</div> \
               </div> \
           </div> \
           <div class="col-sm-6 col-lg-4 m-b-0"> \
@@ -572,7 +576,6 @@ angular.module('tw.styleguide.docs', [])
                       <input class="form-control text-xs-right p-r-0" type="text"> \
                       <span class="input-group-addon p-l-1 ">USD</span> \
                   </div> \
-                  <div class="help-block m-b-0 ">Fee £1.00 USD</div> \
               </div> \
           </div> \
           <div class="col-sm-12 col-lg-4 p-t-3 m-b-0"> \
