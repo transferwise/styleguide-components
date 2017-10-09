eb8465d84a7367d19c44372dd154104b1a84dd54
Mircea Staicu <md.staicu@yahoo.com>
2017-10-09 11:56:30 +0100

    Add popover correct positioning.
    * Compute popover correct position on the scroll offset
    * Compute popover position when resizing the viewport

--- src/help/pop-over/pop-over.directive.js
+++ src/help/pop-over/pop-over.directive.js
@@ -1,12 +1,11 @@
-
-function PopOver() {
+export default function () {
   return {
     restrict: 'A',
-    link: PopOverLink
+    link: PopOverLink,
   };
 }
 
-function PopOverLink(scope, $element) {
+function PopOverLink($scope, $element) {
   /*
   if (!element.popover) {
     // eslint-disable-next-line no-console
@@ -34,12 +33,12 @@ function PopOverLink(scope, $element) {
   tag.setAttribute('role', 'button');
   tag.setAttribute('data-toggle', 'popover');
   */
+  const element = $element[0];
   const options = {
     trigger: 'focus',
-    placement: 'bottom'
+    placement: 'right',
+    element,
   };
-
-  const element = $element[0];
 
   if (element.getAttribute('data-trigger') === 'hover') {
     options.trigger = 'hover focus'; // TODO we don't support this, remove??
@@ -64,32 +63,205 @@ function PopOverLink(scope, $element) {
   element.setAttribute('role', 'button');
   element.setAttribute('data-toggle', 'popover');
 
-  const body = document.getElementsByTagName('body')[0];
+  const [elementCallback, documentCallback, windowResizeCallback] = getDirectiveCallbacks(options);
 
-  element.addEventListener('click', () => {
-    const popover = document.createElement('div');
-    popover.classList.add('popover');
-    popover.classList.add('in');
-    popover.classList.add(options.placement);
-    popover.setAttribute('role', 'popover');
+  element.addEventListener('click', elementCallback);
+  document.documentElement.addEventListener('click', documentCallback, true);
+  window.addEventListener('resize', windowResizeCallback);
 
-    popover.innerHTML = `
+  $scope.$on('destroy', () => {
+    element.removeEventListener('click', elementCallback);
+    document.documentElement.removeEventListener('click', documentCallback);
+    window.removeEventListener('resize', windowResizeCallback);
+  });
+}
+
+function getDirectiveCallbacks(options) {
+  let popover;
+
+  function getPopover() {
+    const popoverContainer = document.createElement('div');
+
+    popoverContainer.classList.add('popover');
+    popoverContainer.classList.add('in');
+    popoverContainer.classList.add(options.placement);
+
+    popoverContainer.setAttribute('role', 'popover');
+
+    popoverContainer.innerHTML = `
       <div class="arrow"></div>
       <h3 class="popover-title">${options.title}</h3>
       <div class="popover-content">
         ${options.content}
       </div>`;
 
-    body.appendChild(popover);
+    return popoverContainer;
+  }
 
-    const rectangle = element.getBoundingClientRect();
+  function setPopoverPosition() {
+    popover.setAttribute('style', 'display:block; visibility:hidden;');
 
-    // TODO position the popover correctly
-    const offsetX = rectangle.left; // element.offsetWidth +
-    const offsetY = rectangle.top; // element.offsetHeight +
+    const {
+      offsetX,
+      offsetY,
+    } = getPopoverPosition(options, popover);
 
-    popover.setAttribute('style', `display: block; left: ${offsetX}px; top: ${offsetY}px`);
-  });
+    popover.setAttribute('style', `display:block; visibility:visible; top:${offsetY}px; left:${offsetX}px`);
+  }
+
+  function elementCallback() {
+    const body = document.getElementsByTagName('body')[0];
+    const popoverAppended = Array.from(body.children).includes(popover);
+
+    /**
+     If the popover doesn't exist, create, append it and display it
+     */
+    if (!popoverAppended) {
+      popover = getPopover();
+
+      body.appendChild(popover);
+
+      setPopoverPosition();
+    }
+
+    /**
+     Otherwise display it
+     */
+    if (!isElementVisible(popover)) {
+      togglePopoverVisibility(true);
+    }
+  }
+
+  function documentCallback(event) {
+    if (popover) {
+      const clickedOutsidePopover = !popover.contains(event.target);
+
+      if (clickedOutsidePopover) {
+        togglePopoverVisibility(false);
+      }
+    }
+  }
+
+  function windowResizeCallback() {
+    if (popover && isElementVisible(popover)) {
+      setPopoverPosition();
+    }
+  }
+
+  function togglePopoverVisibility(isVisible) {
+    const popoverVisibility = (isVisible) ? 'block' : 'none';
+
+    popover.style.display = popoverVisibility;
+  }
+
+  function isElementVisible(element) {
+    return element.offsetWidth > 0 || element.offsetHeight > 0;
+  }
+
+  return [elementCallback, documentCallback, windowResizeCallback];
 }
 
-export default PopOver;
+function getPopoverPosition(options, popover) {
+  /**
+   * The element's coordinates, for which we want to display the popover
+   */
+  const {
+    offsetX,
+    offsetY,
+  } = getBoundingOffset(options.element);
+
+  /**
+   * The element's size, for which we want to display the popover
+   */
+  const {
+    offsetWidth,
+    offsetHeight,
+  } = getDimensionsOffset(options.element);
+
+  /**
+   * Popover's default coordinates
+   */
+  let popoverOffsets = {
+    offsetX: 0,
+    offsetY: 0,
+  };
+
+  const popoverDimensions = getDimensionsOffset(popover);
+  const popoverStyles = getComputedStyle(popover, null);
+
+  const popoverTopBorder = parseInt(popoverStyles.getPropertyValue('border-top'), 10);
+  const popoverBottomBorder = parseInt(popoverStyles.getPropertyValue('border-bottom'), 10);
+
+  const popoverArrow = popover.querySelector('.arrow');
+  const popoverArrowStyles = getComputedStyle(popoverArrow, null);
+
+  const popoverArrowTopOffset = parseInt(popoverArrowStyles.getPropertyValue('top'), 10);
+  const popoverArrowHeight = parseInt(popoverArrowStyles.getPropertyValue('height'), 10);
+  const popoverArrowMarginTop = parseInt(popoverArrowStyles.getPropertyValue('margin-top'), 10);
+
+  if (options.placement === 'top') {
+    popoverOffsets = {
+      offsetX: (offsetX - (popoverDimensions.offsetWidth / 2)) + (offsetWidth / 2),
+      offsetY: (offsetY - popoverDimensions.offsetHeight - 5),
+    };
+  }
+
+  if (options.placement === 'right') {
+    const popoverOffsetX = offsetX + offsetWidth + 5;
+    const popoverOffsetY = (offsetY - (popoverArrowTopOffset + popoverArrowMarginTop +
+      (popoverArrowHeight / 2))) + ((offsetHeight / 2) -
+      (popoverTopBorder + popoverBottomBorder));
+
+    popoverOffsets = {
+      offsetX: popoverOffsetX,
+      //
+      /* offsetY: (offsetY - (popoverDimensions.offsetHeight / 2))
+      + (offsetHeight / 2), //Center to center
+      */
+      offsetY: popoverOffsetY,
+    };
+  }
+
+  if (options.placement === 'bottom') {
+    popoverOffsets = {
+      offsetX: (offsetX - (popoverDimensions.offsetWidth / 2)) + (offsetWidth / 2),
+      offsetY: (offsetY + offsetHeight + 5),
+    };
+  }
+
+  if (options.placement === 'left') {
+    const popoverOffsetX = (offsetX - (popoverDimensions.offsetWidth - 5));
+    const popoverOffsetY = (offsetY - (popoverArrowTopOffset + popoverArrowMarginTop +
+      (popoverArrowHeight / 2))) + ((offsetHeight / 2) -
+      (popoverTopBorder + popoverBottomBorder));
+
+    popoverOffsets = {
+      offsetX: popoverOffsetX,
+      //
+      /* offsetY: (offsetY - (popoverDimensions.offsetHeight / 2))
+      + (offsetHeight / 2), //Center to center
+      */
+      offsetY: popoverOffsetY,
+    };
+  }
+
+  return popoverOffsets;
+}
+
+function getBoundingOffset(element) {
+  const elementRect = element.getBoundingClientRect();
+  const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  return {
+    offsetY: elementRect.top + scrollTop,
+    offsetX: elementRect.left + scrollLeft,
+  };
+}
+
+function getDimensionsOffset(element) {
+  return {
+    offsetWidth: element.offsetWidth,
+    offsetHeight: element.offsetHeight,
+  };
+}
