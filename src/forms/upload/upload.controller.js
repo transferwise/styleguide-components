b652e921e160479bd8e8bc0e6dc490c1f186d075
Tomás Silva Queiroga <33726822+tomasqueirogabr@users.noreply.github.com>
2018-01-18 12:23:59 +0200

    Fix upload async failure (#160)
    Fix upload failure callback when file is too big

--- src/forms/upload/upload.controller.js
+++ src/forms/upload/upload.controller.js
@@ -68,20 +68,20 @@ class UploadController {
 
     if (!isSizeValid(file, this.maxSize)) {
       this.isTooLarge = true;
-      this.asyncFailure({
+      asyncFailure({
         status: 413,
         statusText: 'Request Entity Too Large'
-      });
+      }, this);
       return;
     }
 
     /*
     if (!isTypeValid(file, this.accept)) {
       this.isWrongType = true;
-      this.asyncFailure({
+      asyncFailure({
         status: 415,
         statusText: 'Unsupported Media Type'
-      });
+      }, this);
       return;
     }
     */
