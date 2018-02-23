1c6471ac4f094da1963da2e6659d2a99ad9cc575
Jerome Lapostolet <jerome.lapostolet@transferwise.com>
2018-02-23 15:18:14 +0200

    Remove redundant errors on tw-telephone

--- src/forms/telephone/index.js
+++ src/forms/telephone/index.js
@@ -1,6 +1,11 @@
 import angular from 'angular';
 import TelephoneControl from './telephone.component.js';
+import DomService from '../../services/dom/';
+import LocaleService from '../../services/locale/';
 
 export default angular
-  .module('tw.styleguide.forms.telephone', [])
+  .module('tw.styleguide.forms.telephone', [
+    DomService,
+    LocaleService
+  ])
   .component('twTelephone', TelephoneControl).name;
