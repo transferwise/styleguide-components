a66cee0095170ec81773db16dee4ee4c01261a5f
Steve Pole <steve.pole@transferwise.com>
2019-04-05 14:20:18 +0100

    Improve model handling on allOf schemas

--- src/json-schema/all-of-schema/controller.js
+++ src/json-schema/all-of-schema/controller.js
@@ -1,26 +1,53 @@
+import { cleanModel } from '../utils/clean-model';
+
 class Controller {
-  $onInit() {
-    if (!this.model) {
-      this.model = {};
+  $onChanges(changes) {
+    if (changes.schema || changes.model) {
+      // We keep each model separately, and combine them for broadcast, this gives
+      // better control over cleaning up the model as we deal with values that are
+      // removed
+      this.models = [];
+      if (this.schema && this.schema.allOf) {
+        this.models = splitModel(this.model, this.schema.allOf);
+      }
     }
   }
 
-  onModelChange(model) {
-    angular.extend(this.model, model);
+  onModelChange(model, index) {
+    const schemaForIndex = this.schema.allOf[index];
+
+    this.models[index] = cleanModel(model, schemaForIndex);
 
     if (this.onChange) {
-      this.onChange({ model: this.model });
+      this.onChange({ model: combineModels(this.models) });
     }
   }
 
+  onRefreshRequirements(model, index) {
+    // TODO tidy
+    const schemaForIndex = this.schema.allOf[index];
 
-  onRefreshRequirements(model) {
-    angular.extend(this.model, model); // TOOO doing this twice
+    this.models[index] = cleanModel(model, schemaForIndex);
 
     if (this.onRefresh) {
-      this.onRefresh({ model: this.model });
+      this.onRefresh({ model: combineModels(this.models) });
     }
   }
+}
+
+function splitModel(model, schemas) {
+  // If we receive a model, break it down to parts valid for each schema
+  const models = [];
+  if (model && schemas) {
+    schemas.forEach((schema) => {
+      models.push(cleanModel(model, schema) || {});
+    });
+  }
+  return models;
+}
+
+function combineModels(models) {
+  return models.reduce((current, combined) => angular.extend(combined, current), {});
 }
 
 export default Controller;
--- src/json-schema/all-of-schema/spec.js
+++ src/json-schema/all-of-schema/spec.js
@@ -38,10 +38,15 @@ describe('Given a component from rendering allOf schemas', function() {
   describe('when given a single schema', function() {
     beforeEach(function() {
       $scope.schema = {
-        title: "All of the following",
-        description: "Description of stuff",
+        title: 'All of the following',
+        description: 'Description of stuff',
         allOf: [{
-          type: "string",
+          type: 'object',
+          properties: {
+            a: {
+              type: 'number'
+            }
+          },
           width: 'md'
         }]
       };
@@ -69,87 +74,115 @@ describe('Given a component from rendering allOf schemas', function() {
 
     describe('with a model', function() {
       beforeEach(function() {
-        $scope.model = {a: 1};
+        $scope.model = { a: 1 };
         $scope.$apply();
       });
       it('should pass it to a generic schema component', function() {
         expect(genericSchema.bindings.model).toEqual($scope.model);
       });
     });
+  });
 
-    describe('with error messages', function() {
-      beforeEach(function() {
-        $scope.errors = {a:"error"};
-        $scope.$apply();
-      });
-      it('should pass it to a generic schema component', function() {
-        expect(genericSchema.bindings.errors).toEqual($scope.errors);
-      });
+  describe('when given multiple schemas', function() {
+    var genericSchemas, schema1controller, schema2controller;
+
+    beforeEach(function() {
+      $scope.schema = {
+        allOf: [{
+          type: 'object',
+          properties: {
+            a: {
+              type: 'string'
+            }
+          }
+        },{
+          type: 'object',
+          properties: {
+            b: {
+              type: 'number'
+            }
+          }
+        }]
+      };
+
+      $scope.model = {
+        a: 'hello',
+        b: 1,
+        c: 'not in schema'
+      };
+
+      $scope.errors = {
+        a: 'wrong',
+        b: 'wronger'
+      };
+
+      $scope.locale = 'es-ES';
+
+      $scope.translations = {
+        save: 'Save'
+      };
+
+      $scope.$apply();
+
+      genericSchemas = component.querySelectorAll('generic-schema');
+
+      schema1controller = angular.element(genericSchemas[0]).controller('genericSchema');
+      schema2controller = angular.element(genericSchemas[1]).controller('genericSchema');
     });
 
-    describe('with a locale', function() {
-      beforeEach(function() {
-        $scope.locale = 'es-ES';
-        $scope.$apply();
-      });
-      it('should pass it to a generic schema component', function() {
-        expect(genericSchema.bindings.locale).toEqual($scope.locale);
-      });
+    it('should render a generic-schema for each', function() {
+      expect(genericSchemas.length).toBe(2);
     });
 
-    describe('with translations', function() {
-      beforeEach(function() {
-        $scope.translations = {};
-        $scope.$apply();
-      });
-      it('should pass them to a generic schema component', function() {
-        expect(genericSchema.bindings.translations).toEqual($scope.translations);
-      });
+    it('should pass the correct pieces of the schema to each generic-schema', function() {
+      expect(schema1controller.schema).toEqual($scope.schema.allOf[0]);
+      expect(schema2controller.schema).toEqual($scope.schema.allOf[1]);
     });
 
-    describe('when the generic schema triggers an onChange event', function() {
+    it('should pass the correct pieces of the model to each generic-schema', function() {
+      expect(schema1controller.model).toEqual({ a: 'hello' });
+      expect(schema2controller.model).toEqual({ b: 1 });
+    });
+
+    it('should pass the error messages to both generic-schema', function() {
+      expect(schema1controller.errors).toEqual($scope.errors);
+      expect(schema2controller.errors).toEqual($scope.errors);
+    });
+
+    it('should pass the translations to both generic-schema', function() {
+      expect(schema1controller.translations).toEqual($scope.translations);
+      expect(schema2controller.translations).toEqual($scope.translations);
+    });
+
+    it('should pass the locale to both generic-schema', function() {
+      expect(schema1controller.locale).toEqual($scope.locale);
+      expect(schema2controller.locale).toEqual($scope.locale);
+    });
+
+    describe('when a child schema triggers onChange', function() {
       beforeEach(function() {
-        $scope.model = { a: 1 };
-        $scope.$apply();
+        // refers to second generic-schema component
         genericSchema.bindings.onChange({ model: { b: 2 } });
       });
       it('should trigger the components onChange once', function() {
         expect($scope.onChange.calls.count()).toBe(1);
       });
-      it('should combine the changed model with the internal model', function() {
-        expect($scope.onChange).toHaveBeenCalledWith({ a: 1, b: 2 });
+      it('should combine the changed model with the other parts of the model', function() {
+        expect($scope.onChange).toHaveBeenCalledWith({ a: 'hello', b: 2 });
       });
     });
 
-    describe('when the generic schema triggers onRefreshRequirements', function() {
+    describe('when a child schema triggers onRefresh', function() {
       beforeEach(function() {
-        $scope.model = { a: 1 };
-        $scope.$apply();
-        genericSchema.bindings.onRefresh({ model: { b: 2 } });
+        // refers to second generic-schema component
+        genericSchema.bindings.onRefresh({ model: { b: 3 } });
       });
-      it('should propogate that event to consumers', function() {
+      it('should trigger the components onChange once', function() {
         expect($scope.onRefresh.calls.count()).toBe(1);
       });
-      it('should combine the changed model with the internal model', function() {
-        expect($scope.onRefresh).toHaveBeenCalledWith({ a: 1, b: 2 });
+      it('should combine the changed model with the other parts of the model', function() {
+        expect($scope.onRefresh).toHaveBeenCalledWith({ a: 'hello', b: 3 });
       });
-    });
-  });
-
-  describe('when given multiple schemas', function() {
-    beforeEach(function() {
-      $scope.schema = {
-        allOf: [{
-          type: "string"
-        },{
-          type: "number"
-        }]
-      };
-      $scope.$apply();
-    });
-    it('should render a generic-schema for each', function() {
-      var genericSchemas = component.querySelectorAll('generic-schema');
-      expect(genericSchemas.length).toBe(2);
     });
   });
 });
