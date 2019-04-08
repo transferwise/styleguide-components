b2e025e5f6458ea59894081a6c787af64ba3589d
Steve Pole <steve.pole@transferwise.com>
2019-04-08 11:44:41 +0100

    Better initialisation for arrays

--- src/json-schema/array-schema/controller.js
+++ src/json-schema/array-schema/controller.js
@@ -3,10 +3,17 @@ const simpleTypes = ['string', 'number', 'integer', 'boolean'];
 class Controller {
   $onInit() {
     if (!Array.isArray(this.model)) {
-      if (this.schema && this.schema.items && this.isSimpleType(this.schema.items.type)) {
+      const arrayType = this.schema && this.schema.items && this.schema.items.type;
+
+      if (!arrayType || this.isSimpleType(arrayType)) {
+        // If we're dealing with simple types, oneOf or allOf, leave it to them.
         this.model = [];
-      } else {
+      } else if (arrayType === 'object') {
+        // Seed the array with one object, to show one empty form.
         this.model = [{}];
+      } else if (arrayType === 'array') {
+        // Seed the array with one array (I hope we never do this).
+        this.model = [[]];
       }
     }
   }
