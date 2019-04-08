6f32b9a8de3e51332f0fc313443d250fcc9e3952
Steve Pole <steve.pole@transferwise.com>
2019-04-08 11:52:17 +0100

    Improve array schema initialisation further

--- src/json-schema/array-schema/controller.js
+++ src/json-schema/array-schema/controller.js
@@ -3,17 +3,13 @@ const simpleTypes = ['string', 'number', 'integer', 'boolean'];
 class Controller {
   $onInit() {
     if (!Array.isArray(this.model)) {
-      const arrayType = this.schema && this.schema.items && this.schema.items.type;
+      const arrayType = getArrayType(this.schema);
+      const newItem = getNewItem(arrayType);
 
-      if (!arrayType || this.isSimpleType(arrayType)) {
-        // If we're dealing with simple types, oneOf or allOf, leave it to them.
-        this.model = [];
-      } else if (arrayType === 'object') {
-        // Seed the array with one object, to show one empty form.
-        this.model = [{}];
-      } else if (arrayType === 'array') {
-        // Seed the array with one array (I hope we never do this).
-        this.model = [[]];
+      this.model = [];
+      if (newItem) {
+        // Seed the array with a newItem to show one empty form
+        this.model.push(newItem);
       }
     }
   }
@@ -32,12 +28,30 @@ class Controller {
   }
 
   add() {
-    this.model.push({});
+    const arrayType = getArrayType(this.schema);
+    const newItem = getNewItem(arrayType);
+    if (newItem) {
+      this.model.push(newItem);
+    }
   }
 
   isSimpleType(type) { // eslint-disable-line
     return simpleTypes.indexOf(type) >= 0;
   }
+}
+
+function getArrayType(schema) {
+  return schema && schema.items && schema.items.type;
+}
+
+function getNewItem(arrayType) {
+  if (arrayType === 'object') {
+    return {};
+  } else if (arrayType === 'array') {
+    return [];
+  }
+
+  return null;
 }
 
 export default Controller;
