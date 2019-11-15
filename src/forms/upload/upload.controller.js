96d50aada5d6634342bcc0b7f3693a0f33ebe170
Kyle Patel <kyle.patel@transferwise.com>
2019-11-15 15:24:53 +0000

    lin fixes

--- src/forms/upload/upload.controller.js
+++ src/forms/upload/upload.controller.js
@@ -120,7 +120,8 @@ class UploadController {
      * NOTE: We temporarily want to ignore httpOptions until a full solution is implemented
      * Currently httpOptions.url is a relative path, but needs to know its baseUrl
      * As this call will return a 404, stopping a user from continuing the flow.
-     */
+    */
+
     // if (this.httpOptions) {
     //   // Post file now
     //   this.$q
@@ -131,12 +132,11 @@ class UploadController {
     //     .then(response => asyncSuccess(response[0], response[1], this))
     //     .catch(error => asyncFailure(error, this));
     // } else {
-      // Post on form submit
-      this.asyncFileRead(file)
-        .then(response => asyncSuccess(null, response, this))
-        .catch(error => asyncFailure(error, this));
-    //}
-
+    // Post on form submit
+    this.asyncFileRead(file)
+      .then(response => asyncSuccess(null, response, this))
+      .catch(error => asyncFailure(error, this));
+    // }
   }
 
   onDragEnter() {
