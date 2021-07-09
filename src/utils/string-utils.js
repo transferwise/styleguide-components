07643efe0d2014778e2006528f5d1a1864b46ec9
Peter Balogh <peter.balogh@transferwise.com>
2021-07-09 13:41:41 +0200

    style: add JSDOC comment

--- src/utils/string-utils.js
+++ src/utils/string-utils.js
@@ -1,5 +1,11 @@
 /* eslint-disable import/prefer-default-export */
 
+
+/**
+ * Safely returns the lowercased value of a string
+ * @param {string} value
+ * @returns {string}
+ */
 export function safeToLowerCase(value) {
   return typeof value === 'string' ? value.toLowerCase() : value;
 }
